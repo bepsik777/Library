5b30d7c31d454ee83e62fc9ff2751211fab0ae68
bepsik777 <potocki.alex@gmail.com>
2023-02-02 18:08:09 +0100

    Prevent duplicated books and display warning

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+const addBookButton = document.querySelector('.add')
 const formSubmitButton = document.querySelector('.submit')
+const closeFormButton = document.querySelector('.closeButton')
 const title = document.querySelector('#title')
 const author = document.querySelector('#author')
 const pages = document.querySelector('#pages')
 const unfinished = document.querySelector('#unfinished')
 const finished = document.querySelector('#finished')
+const table = document.querySelector('.table')
 const formModal = document.querySelector('.modal')
-const addBookButton = document.querySelector('.add')
-const closeFormButton = document.querySelector('.closeButton')
+const form = document.querySelector('form')
+const warning = document.createElement('p')
 
 console.log(formModal)
 console.log(formSubmitButton)
@@ -16,13 +19,13 @@ const myLibrary = [
     title: 'Lord of The ring',
     author: 'J.R.R. Tolkien',
     pages: 400,
-    finished: 'unfinished'
+    finished: 'finished'
   },
   {
     title: 'Hellblazer',
     author: 'Jamie Delano',
     pages: 250,
-    finished: 'finished'
+    finished: 'unfinished'
   },
   {
     title: 'harry potter',
@@ -32,65 +35,55 @@ const myLibrary = [
   }
 ]
 
-function Book () {
-  this.title = ''
-  this.author = ''
-  this.pages = 0
-  this.finished = ''
-}
-
-// 'THIS' PROBABLY NOT WORKING
-function changeStatus () {
-  if (this.finished.textContent === 'finished') {
-    this.finished.textContent = 'unfinished'
-    this.classList.remove('finished')
-    this.classList.add('unfinished')
-  } else {
-    this.finished.textContent = 'finished'
-    this.classList.remove('unfinished')
-    this.classList.add('finished')
-  }
+function Book (title, author, pages, status) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.finished = status
 }
 
 function addBookToLibrary () {
   const book = Object.create(Book)
-  const statusButton = document.createElement('button')
   book.title = title.value
   book.author = author.value
   book.pages = pages.value
-  book.finished = statusButton
-  function getStatus () {
-    if (unfinished.checked) {
-      book.finished.textContent = unfinished.value
-      book.finished.classList.add('status-button')
-    } else {
-      book.finished.textContent = finished.value
-      book.finished.classList.add('status-button')
-    }
+  if (finished.checked) {
+    book.finished = finished.value
+  } else {
+    book.finished = unfinished.value
   }
-  getStatus()
-  book.finished.addEventListener('click', (e) => {
-    if (e.target.textContent === 'finished') {
-      e.target.textContent = 'unfinished'
-      e.target.classList.remove('finished')
-      e.target.classList.add('unfinished')
-    } else {
-      e.target.textContent = 'finished'
-      e.target.classList.remove('unfinished')
-      e.target.classList.add('finished')
-    }
-  })
   myLibrary.push(book)
 }
 
+// function preventDuplicate () {
+//   const isDuplicate = myLibrary.some(book => book.title === title.value)
+//   if (isDuplicate) {
+//     formSubmitButton.disabled = true
+//   } else {
+//     formSubmitButton.disabled = false
+//   }
+// }
+
+// function addBookToTable () {
+//   myLibrary.forEach((book) => {
+
+//   })
+// }
+
+/*
+1. w funkcji addBookToTable sprawić aby w komórkach z readingStatus pojawiał się guzik oraz żeby dla każdego
+rzędu pojawiała się dodatkowa kolumna z przyciskiem umożliwiającym usunięcie rzędu. (napisać funkcję na nowo?)
+2. Zablokować w funkcji addToLibrary możliwość dodawania książek o o tytułach które się powtarzają
+*/
+
 function addBookToTable () {
-  const table = document.querySelector('.table')
   myLibrary.forEach((book) => {
     const tableRows = Array.from(document.querySelectorAll('tr'))
     const isBook = tableRows.some(row => row.className === book.title)
     const newTableRow = document.createElement('tr')
-    const values = Object.values(book)
+    newTableRow.dataset.index = myLibrary.indexOf(book)
     newTableRow.className = book.title
+    const values = Object.values(book)
     table.appendChild(newTableRow)
     if (isBook === false) {
       values.forEach((value) => {
@@ -118,6 +111,14 @@ function showForm () {
 
 formSubmitButton.addEventListener('click', (e) => {
   e.preventDefault()
+  const isDuplicate = myLibrary.some(book => book.title === title.value)
+  if (isDuplicate) {
+    warning.className = 'duplicate-warning'
+    warning.textContent = 'There is already a book with the same title in the library'
+    form.appendChild(warning)
+    return
+  }
+  warning.remove()
   addBookToLibrary()
   addBookToTable()
   hideForm()
@@ -125,5 +126,8 @@ formSubmitButton.addEventListener('click', (e) => {
 
 addBookButton.addEventListener('click', showForm)
 closeFormButton.addEventListener('click', hideForm)
+closeFormButton.addEventListener('click', () => {
+  warning.remove()
+})
 
 addBookToTable()
